962ed00452ba50a3133536a9a95c3719d6239ee9
jay prakash thakur <jayprakashthakursnr@gmail.com>
2017-01-04 15:42:15 +0530

    UnChecked Exception Handling impl.

--- src/UnCheckedException.java
+++ src/UnCheckedException.java
@@ -1,8 +1,18 @@
 public class UnCheckedException {
     public static void main(String[] args) {
-        System.out.println("Basic Exception Example");
-        int x = 10 / 0; // this line will not be checked at compile time but at Runtime it will throw an exception.
+        System.out.println("This line will be executed");
+        /*int x = 10 / 0; // this line will not be checked at compile time but at Runtime it will throw an exception.
         System.out.println(x);// java.lang.ArithmeticException: / by zero - Exception will occur
-        System.out.println("This line will not be executed");
+        System.out.println("This line will not be executed");*/
+
+        // Handle this UnChecked Exception as -
+        try{
+            int x = 10 / 0;
+            System.out.println(x);
+        }catch (ArithmeticException ae){
+            System.out.println("Exception :- "+ae);
+        }
+
+        System.out.println("Now This line will be executed");
     }
 }
